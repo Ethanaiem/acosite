28f253b0a16afe7709154f36e19dcb175bd96d06
Ethanaiem <85930035+Ethanaiem@users.noreply.github.com>
2026-01-18 11:05:11 -0500

    Add files via upload

---
+++ server/routes.ts
@@ -0,0 +1,21 @@
+import type { Express } from "express";
+import { createServer, type Server } from "http";
+import { storage } from "./storage";
+import { setupAuth, registerAuthRoutes } from "./replit_integrations/auth";
+
+export async function registerRoutes(
+  httpServer: Server,
+  app: Express
+): Promise<Server> {
+  // Setup authentication
+  await setupAuth(app);
+  registerAuthRoutes(app);
+
+  // put application routes here
+  // prefix all routes with /api
+
+  // use storage to perform CRUD operations on the storage interface
+  // e.g. storage.insertUser(user) or storage.getUserByUsername(username)
+
+  return httpServer;
+}
